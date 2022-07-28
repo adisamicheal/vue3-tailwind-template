0ff940cb21ef5af428bb13db515e6912a1ae992b
adisamicheal <adisamicheal20@gmail.com>
2022-07-28 09:33:01 +0100

    vue 3 and tailwind css app setup

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-center p-3">
+    <h1 class="text-green-500 text-3xl">This is a taiwind app</h1>
+    <button class="bg-sky-600 hover:bg-sky-700 text-white p-3 rounded-2xl mt-3">
+      My Tailwind styled button
+    </button>
+  </div>
+</template>
